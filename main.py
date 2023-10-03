a6d9f197d5cadb0e2e43406236a7a7a4e4d836a6
Debiddo <dzheng@terpmail.umd.edu>
2023-10-03 02:37:49 -0400

    final code formatting

--- main.py
+++ main.py
@@ -3,30 +3,31 @@ from backend import Chatbot
 from PyQt6.QtWidgets import QMainWindow, QTextEdit, QLineEdit, QPushButton, QApplication
 import threading
 
+
 class ChatbotWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setMinimumSize(700,500)
+        self.setMinimumSize(700, 500)
 
         self.chatbot = Chatbot()
 
-
         # Add chat area widget
         self.chat_area = QTextEdit(self)
-        self.chat_area.setGeometry(10,10,480,320)
+        self.chat_area.setGeometry(10, 10, 480, 320)
         self.chat_area.setReadOnly(True)
 
         # Add the input field widget
         self.input_field = QLineEdit(self)
-        self.input_field.setGeometry(10,330,480,40)
+        self.input_field.setGeometry(10, 330, 480, 40)
         self.input_field.returnPressed.connect(self.send_message)
 
         # Add the button
         self.button = QPushButton("Send", self)
-        self.button.setGeometry(500,330,80,40)
+        self.button.setGeometry(500, 330, 80, 40)
         self.button.clicked.connect(self.send_message)
         self.show()
+
     def send_message(self):
         user_input = self.input_field.text().strip()
         self.chat_area.append(f"<p>Me: {user_input}</p>")
@@ -38,7 +39,6 @@ class ChatbotWindow(QMainWindow):
     def get_bot_response(self, user_input):
         response = self.chatbot.get_response(user_input)
         self.chat_area.append(f"<p>AI: {response}</p>")
-
 
 
 app = QApplication(sys.argv)
